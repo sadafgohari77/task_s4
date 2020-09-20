a3898ef95ca4ab31950c0b7ff049d5318fc57537
sadafgohari77 <sadafgohari77@yahoo.com>
2020-09-20 11:07:05 +0430

    fist commit

---
+++ js/main.js
@@ -0,0 +1,71 @@
+function buttonStyleClicked(idButton){
+  for(let counter=0;counter<16;counter+=3){
+    let setElement=counter.toString();
+    document.getElementById(setElement).style.borderColor ="#dbdbdb";
+    document.getElementById('icon'+setElement).style.display='none';
+  }
+  let elementClicked=idButton;
+  document.getElementById(elementClicked.toString()).style.borderColor =" #1faa00";
+  document.getElementById('icon'+elementClicked.toString()).style.display='block';
+  setPrice(elementClicked);
+}
+
+function setPrice(idMonthSubscription){
+  let subscriptionRight=9100;
+  let ReceivedCharge=3900;
+  let price=13000;
+  switch(idMonthSubscription){
+    case 0 :
+      price=13000;
+      subscriptionRight=(price*70)/100;
+      ReceivedCharge=(price*30)/100
+      document.getElementById("price").innerHTML=price.toString();
+      document.getElementById("subscriptionRight").innerHTML=subscriptionRight.toString();
+      document.getElementById("ReceivedCharge").innerHTML=ReceivedCharge.toString();
+      break;
+    case 3:
+      price=13000*3;
+      subscriptionRight=(price*70)/100;
+      ReceivedCharge=(price*30)/100
+      document.getElementById("price").innerHTML=price.toString();
+      document.getElementById("subscriptionRight").innerHTML=subscriptionRight.toString();
+      document.getElementById("ReceivedCharge").innerHTML=ReceivedCharge.toString();
+      break;
+    case 6:
+      price = 13000 * 6;
+      subscriptionRight = (price*70)/100;
+      ReceivedCharge=(price*30)/100
+      document.getElementById("price").innerHTML=price.toString();
+      document.getElementById("subscriptionRight").innerHTML=subscriptionRight.toString();
+      document.getElementById("ReceivedCharge").innerHTML=ReceivedCharge.toString();
+      break;
+    case 9:
+      price=13000*9;
+      subscriptionRight=(price*70)/100;
+      ReceivedCharge=(price*30)/100
+      document.getElementById("price").innerHTML=price.toString();
+      document.getElementById("subscriptionRight").innerHTML=subscriptionRight.toString();
+      document.getElementById("ReceivedCharge").innerHTML=ReceivedCharge.toString();
+      break;
+    case 12:
+      price=13000*12;
+      subscriptionRight=(price*70)/100;
+      ReceivedCharge=(price*30)/100
+      document.getElementById("price").innerHTML=price.toString();
+      document.getElementById("subscriptionRight").innerHTML=subscriptionRight.toString();
+      document.getElementById("ReceivedCharge").innerHTML=ReceivedCharge.toString();
+      break;
+    case 15:
+      price = 13000 * 12-((price*12*10)/100);
+      subscriptionRight=(price*70)/100;
+      ReceivedCharge=(price*30)/100
+      document.getElementById("price").innerHTML=price.toString();
+      document.getElementById("subscriptionRight").innerHTML=subscriptionRight.toString();
+      document.getElementById("ReceivedCharge").innerHTML=ReceivedCharge.toString();
+      break;
+  }
+}
+
+
+
+
